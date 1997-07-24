fa75a8416041db3966b288f0dd9622b29e146b6c
KDE Dinosaur kulow <cvsuser@kde.org>
1997-07-24 12:50:10 +0000

    Coolo: changed private to protected, since krn depends on this

--- mimelib/mimelib/nntp.h
+++ mimelib/mimelib/nntp.h
@@ -353,7 +353,7 @@ public:
     //. this may cause problems in the current implementation if a CR LF '.'
     //. sequence is split between calls.
 
-private:
+protected:
 
     char*       mSendBuffer;
     char*       mRecvBuffer;
