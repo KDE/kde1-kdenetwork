cef01f79c29fa4883604146687ec3edd9b728fa0
KDE Dinosaur asj <cvsuser@kde.org>
1997-12-15 00:06:16 +0000

    Main listbox controls.

---
+++ ksirc/irclistbox.cpp
@@ -0,0 +1,88 @@
+#include "irclistbox.h"
+#include <iostream.h>
+
+static const int fudge = 5;
+
+KSircListBox::KSircListBox(QWidget * parent=0, const char * name=0, WFlags f=0) : QListBox(parent,name,f)
+{
+  setAutoScrollBar(FALSE);
+  vertScroll = new QScrollBar(this, "VertScrollBar");
+  vertScroll->setOrientation(QScrollBar::Vertical);
+  vertScroll->resize(16, this->height());
+  vertScroll->move(this->width() - vertScroll->width(), 0);
+  updateScrollBars();
+  connect(vertScroll, SIGNAL(sliderMoved(int)),
+          SLOT(scrollTo(int)));
+  connect(vertScroll, SIGNAL(nextLine()),
+	  SLOT(lineDown()));
+  connect(vertScroll, SIGNAL(prevLine()),
+	  SLOT(lineUp()));
+  connect(vertScroll, SIGNAL(nextPage()),
+	  SLOT(pageDown()));
+  connect(vertScroll, SIGNAL(prevPage()),
+	  SLOT(pageUp()));
+  vertScroll->show();
+}
+
+void KSircListBox::scrollToBottom()
+{
+  // updateTableSize();
+ setYOffset(totalHeight() - height() + fudge);
+ updateScrollBars();
+}
+
+void KSircListBox::updateScrollBars()
+{
+  vertScroll->setRange(0, count());
+  if((int) count() > numItemsVisible()){
+     int scroll = count()*yOffset()/(totalHeight() - height());
+     vertScroll->setValue(scroll);
+  }
+}
+
+void KSircListBox::resizeEvent(QResizeEvent *)
+{
+  vertScroll->resize(16, this->height());
+  vertScroll->move(this->width() - vertScroll->width(), 0);
+}
+
+void KSircListBox::setTopItem(int index)
+{
+  QListBox::setTopItem(index);
+  updateScrollBars();
+}
+
+void KSircListBox::scrollTo(int index)
+{
+   int yoff = index*(totalHeight() - height()) / count() + fudge;
+   setYOffset(yoff);
+}
+
+void KSircListBox::pageUp()
+{
+  setYOffset(QMAX(0, yOffset()-height()));
+  updateScrollBars();
+}
+
+void KSircListBox::pageDown()
+{
+  setYOffset(QMIN(totalHeight()-height()+fudge, yOffset()+height()));
+  updateScrollBars();
+}
+
+void KSircListBox::lineUp()
+{
+  setYOffset(QMAX(0, yOffset()-itemHeight(topItem())));
+  updateScrollBars();
+}
+
+void KSircListBox::lineDown()
+{
+  setYOffset(QMIN(totalHeight()-height()+fudge, yOffset()+itemHeight(topItem())));
+  updateScrollBars();
+}
+
+//void KSircListBox::scrollBarUpdates(bool update)
+//{
+//  verticalScrollBar()->setUpdatesEnabled(update);
+//}
---
+++ ksirc/irclistbox.h
@@ -0,0 +1,33 @@
+#include <qobject.h>
+#include <qlistbox.h>
+#include <qscrbar.h>
+
+class KSircListBox : public QListBox
+{
+ Q_OBJECT;
+ public:
+  KSircListBox(QWidget * parent=0, const char * name=0, WFlags f=0);
+
+  virtual void scrollToBottom();
+
+//  virtual void scrollBarUpdates(bool);
+
+ public slots:
+  virtual void updateScrollBars();
+  virtual void setTopItem(int);
+
+  virtual void scrollTo(int);
+
+  virtual void pageDown();
+  virtual void pageUp();
+  virtual void lineUp();
+  virtual void lineDown();
+
+ protected:
+  virtual void resizeEvent(QResizeEvent *);
+
+ private:
+
+  QScrollBar *vertScroll;
+
+};
