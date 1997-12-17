440e0090a7ff7c4849d5ef3eb10e9c60003bcc68
KDE Dinosaur asj <cvsuser@kde.org>
1997-12-17 20:49:59 +0000

    A new ioDiscard object.  Everything written to it just thrown away.
    Use it in conjunction with the filter rules to delete lines.

---
+++ ksirc/ioDiscard.cpp
@@ -0,0 +1,24 @@
+/**********************************************************************
+ 
+ The IO Discarder
+
+ $$Id$$
+
+ Simple rule, junk EVERYTHING!!!
+
+**********************************************************************/
+ 
+
+#include "ioDiscard.h"
+
+KSircIODiscard::~KSircIODiscard()
+{
+}
+
+void KSircIODiscard::sirc_receive(QString)
+{
+}
+
+void KSircIODiscard::control_message(QString)
+{
+}
