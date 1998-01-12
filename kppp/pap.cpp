fb0047484eaacf194c0e6da9ede6ff1d0a70423f
KDE Dinosaur mario <cvsuser@kde.org>
1998-01-12 21:02:22 +0000

    Mario: PAP support

---
+++ kppp/pap.cpp
@@ -0,0 +1,71 @@
+/*
+ *
+ *              kPPP: A pppd Front End for the KDE project
+ *              Copyright (C) 1997 Bernd Johannes Wuebben
+ *                      wuebben@math.cornell.edu
+ * 
+ * This file was contributed by Mario Weilguni <mweilguni@sime.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <unistd.h>
+#include "pap.h"
+#include "pppdata.h"
+#include "homedir.h"
+
+bool PAP_UsePAP() {
+  return (bool)(gpppdata.authMethod() == AUTH_PAP);
+}
+
+bool PAP_CreateAuthFile() {  
+  // Create the pap authentication file. PPPD requires that the file is
+  // owned by the real user. If we are running SETUID root, simply make
+  // a chown on it
+  QString fname = getHomeDir() + PAP_AUTH_FILE;
+  int fd = open(fname.data(), 
+		O_CREAT|O_TRUNC|O_WRONLY,
+		S_IRUSR|S_IWUSR);
+  if(fd > 0) {
+    bool err = false;
+    QString s = gpppdata.ID;
+    s += "\n";
+    err |= (write(fd, s.data(), s.length()) != (int)s.length());
+
+    s = gpppdata.password;
+    s += "\n";
+    err |= (write(fd, s.data(), s.length()) != (int)s.length());
+    
+    // for SETUID root
+    if(getuid() != geteuid() && geteuid() == 0)
+      err |= (fchown(fd, getuid(), (gid_t)-1) != 0);
+
+    err |= (close(fd) != 0);
+    return !err;
+  }
+
+  return FALSE;
+}
+
+bool PAP_RemoveAuthFile() {
+  if(!PAP_UsePAP())
+    return FALSE;
+
+  QString fname = getHomeDir() + PAP_AUTH_FILE;
+  if(access(fname.data(), F_OK) == 0)
+    return (bool)(unlink(fname.data()) != -1);
+  else
+    return FALSE;
+}
---
+++ kppp/pap.h
@@ -0,0 +1,42 @@
+/*
+ *
+ *              kPPP: A pppd Front End for the KDE project
+ *
+ * $Id$
+ *              Copyright (C) 1997 Bernd Johannes Wuebben
+ *                      wuebben@math.cornell.edu
+ *
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+
+#ifndef _PAP_H_
+#define _PAP_H_
+
+#include <sys/types.h>
+#include <signal.h>
+
+#define AUTH_SCRIPT     0
+#define AUTH_PAP        1
+
+// file used for PAP authetication purposes
+#define PAP_AUTH_FILE ".kde/share/apps/kppp/.pap-auth"
+
+bool PAP_UsePAP();
+bool PAP_CreateAuthFile();
+bool PAP_RemoveAuthFile();
+
+#endif
