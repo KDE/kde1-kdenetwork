c84d53551dd45bed070236843e7184f04127131e
KDE Dinosaur ralsina <cvsuser@kde.org>
1998-02-20 23:58:15 +0000

    [Roberto] Bumped version to 0.2.9, so scoring will be 0.3.0

--- krn/groupdlg.cpp
+++ krn/groupdlg.cpp
@@ -174,15 +174,16 @@ Inherited (name)
     options->insertItem(klocale->translate("Printing Options..."),CONFIG_PRINT);
     connect (options,SIGNAL(activated(int)),SLOT(currentActions(int)));
 
-    /*
+
     QPopupMenu *help = new QPopupMenu;
     help->insertItem(klocale->translate("Contents"),HELP_CONTENTS);
     help->insertSeparator();
     help->insertItem(klocale->translate("About Krn..."),HELP_ABOUT);
     connect (help,SIGNAL(activated(int)),SLOT(currentActions(int)));
-    */
 
-    QPopupMenu *help = kapp->getHelpMenu(true,"Krn 0.2.5");
+    /*
+     QPopupMenu *help = kapp->getHelpMenu(true,"Krn 0.2.5");
+    */
     
     KMenuBar *menu = new KMenuBar (this, "menu");
     
