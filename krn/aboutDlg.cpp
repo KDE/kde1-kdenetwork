c62a394860329787df41cacf9d1c69795242885e
KDE Dinosaur ralsina <cvsuser@kde.org>
1998-04-02 22:15:52 +0000

    [Roberto] Forgot: bumped version to 0.3.5

--- krn/aboutDlg.cpp
+++ krn/aboutDlg.cpp
@@ -34,7 +34,7 @@ aboutDlg::aboutDlg
 
     l->addButton ("logo",p);
     l->newLine();
-    l->addLabel("l3","0.3.0");
+    l->addLabel("l3","0.3.5");
     l->newLine();
     l->addLabel("l1",klocale->translate("Part of the KDE project."));
     l->newLine();
