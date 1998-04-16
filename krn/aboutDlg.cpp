4d402da1d811a317fc87f2e73c02ac37e3be38cd
KDE Dinosaur ralsina <cvsuser@kde.org>
1998-04-16 20:10:25 +0000

    [Roberto] Warped version to 0.4.0, which will be the Beta4 version.

--- krn/aboutDlg.cpp
+++ krn/aboutDlg.cpp
@@ -34,7 +34,7 @@ aboutDlg::aboutDlg
 
     l->addButton ("logo",p);
     l->newLine();
-    l->addLabel("l3","0.3.5");
+    l->addLabel("l3","0.4.0");
     l->newLine();
     l->addLabel("l1",klocale->translate("Part of the KDE project."));
     l->newLine();
