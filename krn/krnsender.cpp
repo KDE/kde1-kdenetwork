e2a7b0190c60916d422b6bf52a4baf79eb2d40f0
KDE Dinosaur ralsina <cvsuser@kde.org>
1998-04-30 16:00:13 +0000

    * [Roberto] Fixed Message-ID bug.             It now uses <time(NULL).rand()@server.hostname> That should             please anyone.

--- krn/krnsender.cpp
+++ krn/krnsender.cpp
@@ -1,6 +1,9 @@
 #include "krnsender.h"
 
 #include <unistd.h>
+#include <sys/utsname.h>
+#include <stdlib.h>
+#include <time.h>
 #include <kmsgbox.h>
 #include <kapp.h>
 
@@ -21,6 +24,9 @@ bool KRNSender::send(KMMessage *aMsg, short sendnow=-1)
     readConfig();
     debug ("KRNSender::send");
     aMsg->cleanupHeader();
+    //Create a better Message-ID
+
+//    aMsg->setHeaderField("Message-ID",
     if (sendnow==-1 && server->isConnected())
         sendnow=1;
     else if (sendnow==-1)
@@ -40,7 +46,12 @@ bool KRNSender::doSendNNTP (KMMessage *msg)
     //Empty bodies make news servers and readers go insane
     if (msg->body().isEmpty())
         msg->setBody("\n\n\n");
-    
+
+    QString t;
+    srand((int)time(NULL));
+    t.sprintf("<%d.%d@%s>",time(NULL),rand(),server->hostname.data());
+    debug ("posting with ID=%s",t.data());
+    msg->setHeaderField("Message-ID",t.data());
     msgStr = msg->asString();
     if (server->isReadOnly())
     {
