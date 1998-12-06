44ac3f37d7859c7bf5a65e8d1c7b8df0deb79f73
KDE Dinosaur bieker <cvsuser@kde.org>
1998-12-06 12:10:28 +0000

    Adds some separators, &s, and comment out the part which sets the caption of the program (we do that on the command line, don't we?)
    This also fixes the Help menu a bit.

--- kfinger/kfinger.C
+++ kfinger/kfinger.C
@@ -20,21 +20,23 @@ NetutilView::NetutilView(const char *name )
 {    
 
 protocol = 0L;
-  setCaption( klocale->translate("KDE finger utility") );
+//  setCaption( klocale->translate("KDE finger utility") );
   app=KApplication::getKApplication();
   config=app->getConfig();
 
   menubar = new KMenuBar( this, "menubar" );
    QPopupMenu *file = new QPopupMenu;
-    file->insertItem(klocale->translate("Finger Now"), this, SLOT(button1Clicked()), CTRL+Key_F);
-    file->insertItem(klocale->translate("Talk.."),this,SLOT(talk()),CTRL+Key_T);
-    file->insertItem(klocale->translate("Quit"), qApp, SLOT(quit()), ALT+Key_Q);
+    file->insertItem(klocale->translate("F&inger Now"), this, SLOT(button1Clicked()), CTRL+Key_F);
+    file->insertItem(klocale->translate("&Talk..."),this,SLOT(talk()),CTRL+Key_T);
+    file->insertSeparator();
+    file->insertItem(klocale->translate("&Quit"), qApp, SLOT(quit()), ALT+Key_Q);
    QPopupMenu *options = new QPopupMenu;
     options->insertItem(klocale->translate("Setup"), this, SLOT(setup()), CTRL+Key_S );
-    options->insertItem(klocale->translate("Save Options"), this, SLOT(save()));
+    options->insertItem(klocale->translate("&Save Options"), this, SLOT(save()));
    QPopupMenu *help = new QPopupMenu;
-    help->insertItem(klocale->translate("Help"), this, SLOT(HtmlHelp()), CTRL+Key_H );
-    help->insertItem(klocale->translate("About"), this, SLOT(about()) );
+    help->insertItem(klocale->translate("&Contents"), this, SLOT(HtmlHelp()), CTRL+Key_H );
+    help->insertSeparator();
+    help->insertItem(klocale->translate("&About kfinger..."), this, SLOT(about()) );
   menubar->insertItem( klocale->translate("&File"), file );
   menubar->insertItem( klocale->translate("&Options"), options, ALT+Key_O );
   menubar->insertSeparator();
