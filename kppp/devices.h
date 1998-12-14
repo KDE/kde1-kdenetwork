d44709aa17e375ef04d10d9b21de3b6093e05cc4
KDE Dinosaur porten <cvsuser@kde.org>
1998-12-14 02:42:48 +0000

    Harri: added device list

---
+++ kppp/devices.h
@@ -0,0 +1,30 @@
+#ifndef _DEVICES_H_
+#define _DEVICES_H_
+
+static char *devices[] = {
+#ifdef __FreeBSD__
+  "/dev/cuaa0",
+  "/dev/cuaa1",
+  "/dev/cuaa2",
+  "/dev/cuaa3",
+#else
+  "/dev/modem",
+  "/dev/cua0",
+  "/dev/cua1",
+  "/dev/cua2",
+  "/dev/cua3",
+  "/dev/ttyS0",
+  "/dev/ttyS1",
+  "/dev/ttyS2",
+  "/dev/ttyS3",
+
+#ifdef ISDNSUPPORT
+  "/dev/ttyI0",
+  "/dev/ttyI1",
+  "/dev/ttyI2",
+  "/dev/ttyI3",
+#endif
+#endif
+  0};
+
+#endif
