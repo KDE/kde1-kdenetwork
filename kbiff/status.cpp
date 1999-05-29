ce5f17759dd865aa72d7291f3b22f63449a26a5b
Helio Chissini de Castro <helio@kde.org>
1999-05-29 05:11:29 +0000

    This commit was manufactured by cvs2svn to create branch 'KDE_1_1_BRANCH'.
    Cherrypick from master 1999-05-29 05:11:28 UTC granroth 'o Ported 99% of latest stable code to cvs version.  It is very nearly usable':
        debian/undocumented
        kbiff/kbiffurl.cpp
        kbiff/pics/mini-noconn.xpm
        kbiff/pics/noconn.xpm
        kbiff/pics/playsound.xpm
        kbiff/status.cpp
        kbiff/status.h

---
+++ kbiff/status.cpp
@@ -0,0 +1,102 @@
+/*
+ * status.cpp
+ * Copyright (C) 1999 Kurt Granroth <granroth@kde.org>
+ *
+ * This file contains the implementation of the KBiffStatus
+ * widget
+ *
+ * $Id$
+ */
+#include "status.h"
+
+#include "Trace.h"
+#include <kapp.h>
+
+#include <qlistview.h>
+#include <qheader.h>
+#include <qlayout.h>
+#include <qlabel.h>
+#include <qpoint.h>
+#include <klocale.h>
+
+KBiffStatus::KBiffStatus(const QString& profile, const KBiffStatusList& list)
+	: QFrame(0, 0, WStyle_Customize | WStyle_NoBorder),
+	  _listView(new QListView(this))
+{
+TRACEINIT("KBiffStatus::KBiffStatus()");
+	setFrameStyle(WinPanel|Raised);
+	QLabel *profile_label = new QLabel(profile, this);
+	profile_label->setFrameStyle(QFrame::Box | QFrame::Raised);
+	profile_label->setAlignment(AlignCenter);
+
+	_listView->addColumn(i18n("Mailbox"));
+	_listView->addColumn(i18n("New"));
+	_listView->setColumnAlignment(1, AlignRight);
+	_listView->setSorting(1, FALSE);
+	_listView->setFrameStyle(QFrame::WinPanel | QFrame::Raised);
+	_listView->setVScrollBarMode(QScrollView::AlwaysOff);
+	_listView->setHScrollBarMode(QScrollView::AlwaysOff);
+	_listView->header()->hide();
+
+	KBiffStatusList *ptr = &list;
+	for(unsigned int i = 0; i < ptr->count(); i++)
+	{
+		const KBiffStatusItem *item = ptr->at(i);
+		new QListViewItem(_listView, item->mailbox(), item->newMessages());
+	}
+
+	_listView->setMinimumSize(_listView->sizeHint());
+	int list_height = (_listView->firstChild()->height() * ptr->count()) + 10;
+	_listView->setMinimumSize(_listView->sizeHint().width() + 5, list_height);
+
+	QVBoxLayout *layout = new QVBoxLayout(this, 0, 0);
+	layout->addWidget(profile_label);
+	layout->addWidget(_listView);
+}
+
+KBiffStatus::~KBiffStatus()
+{
+}
+
+void KBiffStatus::popup(const QPoint& pos)
+{
+	QWidget *desktop = KApplication::desktop();
+	int x = 0, y = 0;
+
+	// verify that the width is within the desktop
+	if ((pos.x() + width()) > desktop->width())
+	{
+		x = pos.x() - width();
+		x = (x < 0) ? 0 : x;
+	}
+
+	// verify that that height is within tolerances
+	if ((pos.y() + height()) > desktop->height())
+	{
+		//y = desktop->height() - height();
+		y = pos.y() - height();
+		y = (y < 0) ? 0 : y;
+	}
+
+	// now that we have *real* co-ordinates, we show it
+	move(x, y);
+	show();
+}
+
+KBiffStatusItem::KBiffStatusItem(const QString& mailbox, const int num_new)
+	: _mailbox(new QString(mailbox)),
+	  _newMessages(new QString(QString().setNum(num_new)))
+{
+}
+
+KBiffStatusItem::~KBiffStatusItem()
+{
+	if (_mailbox) {
+		delete _mailbox;
+		_mailbox = 0;
+	}
+	if (_newMessages) {
+		delete _newMessages;
+		_newMessages = 0;
+	}
+}
---
+++ kbiff/status.h
@@ -0,0 +1,53 @@
+/*
+ * status.h
+ * Copyright (C) 1999 Kurt Granroth <granroth@kde.org>
+ *
+ * This file contains the declaration of the KBiffStatus
+ * widget.
+ *
+ * $Id$
+ */
+#ifndef KBIFFSTATUS_H 
+#define KBIFFSTATUS_H 
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif 
+
+#include <qframe.h>
+#include <qlist.h>
+#include <qstring.h>
+
+class QListView;
+class QPoint;
+
+class KBiffStatusItem
+{
+public:
+	KBiffStatusItem(const QString& mailbox, const int num_new);
+	virtual ~KBiffStatusItem();
+
+	QString mailbox() const { return *_mailbox; }
+	QString newMessages() const { return *_newMessages; }
+
+protected:
+	QString *_mailbox;
+	QString *_newMessages;
+};
+
+typedef QList<KBiffStatusItem> KBiffStatusList;
+
+class KBiffStatus : public QFrame
+{
+	Q_OBJECT
+public:
+	KBiffStatus(const QString& profile, const KBiffStatusList& list);
+	virtual ~KBiffStatus();
+
+	void popup(const QPoint& pos);
+
+protected:
+	QListView *_listView;
+};
+
+#endif // KBIFFSTATUS_H
