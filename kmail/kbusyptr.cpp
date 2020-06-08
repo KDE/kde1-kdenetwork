279cf82aa6a26ce688af594a957479fedb7db595
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2020-06-08 15:37:16 +0200

    fix kmail build

--- kmail/kbusyptr.cpp
+++ kmail/kbusyptr.cpp
@@ -142,7 +142,7 @@ void KBusyPtr :: loadCursor (const char* cursorName,const char* maskName)
 
   if (bitmapList) delete[] bitmapList;
   QSize size(16,16);
-  bitmapList = new QBitmap[numCursors](size);
+  bitmapList = new QBitmap[numCursors];//(size); they are assigned below, I think this should work? Compile fails without the change
   if (cursorList) delete[] cursorList;
   cursorList = new QCursor[numCursors];
 
